8a863c0ea67f62b565b9579d38fe490d085e420d
Marcus Hudnell <103913070+hudnellmarcus@users.noreply.github.com>
2025-05-10 18:26:03 -0400

    header finished

---
+++ src/components/Header.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="flex w-full bg-iheart-red text-white px-4 py-4">
+      <div className="flex items-center justify-between w-full py-2">
+        <div className="flex items-center">
+          <Image
+            src="/iHeartRadio_Logo_iHR Horizontal Black 2.png"
+            alt="logo"
+            className="ml-6 mr-6"
+            width={100}
+            height={100}
+          />
+          <div className="flex px-4 space-x-5">
+            <Link href="/">Offers</Link>
+            <Link href="/">Benefits</Link>
+            <Link href="/">Perks</Link>
+            <Link href="/">Shop</Link>
+            <Link href="/">Redeemed Rewards</Link>
+          </div>
+        </div>
+        <div className="flex items-center space-x-4">
+          <div className="flex items-center space-x-2 px-4">
+            <Image
+              src="/ri_question-fill.png"
+              alt="question"
+              className=""
+              width={25}
+              height={25}
+            />
+            <Image
+              src="/ri_notification-2-fill.png"
+              alt="notification"
+              className=""
+              width={25}
+              height={25}
+            />
+          </div>
+          <div className="flex items-center space-x-2 px-4">
+            <div className="rounded-full bg-white py-2 px-2">
+              <h2 className="text-black">JW</h2>  
+            </div>
+              <h2 className="text-sm px-2">Jillian W.</h2>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Add your custom colors here
+        'iheart-red': '#C6002B',
+        // Add more as needed
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
