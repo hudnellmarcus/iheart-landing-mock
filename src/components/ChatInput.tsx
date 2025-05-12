1be808885bee5a38dcaa82cdd682db1da70f25bf
Marcus Hudnell <103913070+hudnellmarcus@users.noreply.github.com>
2025-05-11 23:23:24 -0400

    chat input done

---
+++ src/components/ChatInput.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+const ChatInput = () => {
+  return (
+    <div className="flex justify-between bg-gray-400/30 py-3 rounded-lg w-[50vw]">
+      <div className="flex px-4 gap-4">
+        <Image src="/Image.png" alt="image icon" width={20} height={20} />
+        <Image src="/microphone.png" alt="microhpone" width={28} height={28} />
+      </div>
+      <input type="text" placeholder="Type message" className="w-full outline-none" />
+      <button type="submit">
+        <Image
+          src="/PaperPlaneRight.png"
+          alt="send"
+          width={24}
+          height={24}
+          className="mr-4"
+        />
+      </button>
+    </div>
+  );
+};
+
+export default ChatInput;
--- src/components/ChatInterface.tsx
+++ src/components/ChatInterface.tsx
@@ -1,21 +1,31 @@
 import ProductDisplay from "./ProductDisplay";
 import { sunglasses, premiumLipsticks, budgetLipsticks } from "@/data/products";
 import Sidebar from "./Sidebar";
+import Chat from "@/app/chat/page";
+import ChatInput from "./ChatInput";
 
 const ChatInterface = () => {
   return (
     <div className="flex">
       <Sidebar />
 
-     {/* <ProductDisplay
+      <div className="flex flex-col items-center h-auto">
+        <div className="flex mx-auto">
+          {/* <ProductDisplay
         title="💸 😎 Here are some stylish sunglasses thatiHeart listeners are buying!"
         products={sunglasses} /> */}
-     {/* <ProductDisplay
+          {/* <ProductDisplay
         title="🤑 💄 Got it! Here are some Red Lipsticks that iHeart listeners are buying!"
         products={premiumLipsticks} /> */}
-    <ProductDisplay
-        title="🤑 💄 No problem! Here are some Red Lipsticks at a lower price"
-        products={budgetLipsticks} />
+          <ProductDisplay
+            title="🤑 💄 No problem! Here are some Red Lipsticks at a lower price"
+            products={budgetLipsticks}
+          />
+        </div>
+        <div>
+          <ChatInput />
+        </div>
+      </div>
     </div>
   );
 };
